1965a277858806b8c765a5c489267ec038283f82
akane9506 <akane19950611@gmail.com>
2026-07-13 18:33:14 -0700

    implement procedual edge drawing

--- src/components/graph/graph.ts
+++ src/components/graph/graph.ts
@@ -8,6 +8,7 @@ type NodeRenderer = {
   y: number;
   active: boolean; //use this to control fadeout
   branches: number;
+  node: GraphNode;
 };
 
 type EdgeRenderer = {
@@ -62,25 +63,18 @@ class GraphNode {
   }
 }
 
-// create nodes
-const head = new GraphNode("head", "io", 100, 300, []);
-const chat1 = new GraphNode("chat1", "chat", 440, 200, []);
-const branch1 = new GraphNode("branch1", "branch", 400, 420, [], 2);
-const state1 = new GraphNode("state1", "state", 560, 360, []);
-const lambda1 = new GraphNode("lambda1", "lambda", 700, 320, []);
-
-//  create graph
-head.children = [chat1, branch1];
-branch1.children = [state1];
-chat1.children = [lambda1];
-state1.children = [lambda1];
-
 // add parents to each node
-const buildRelationship = (head: GraphNode) => {
-  head.children.forEach((child) => {
-    child.parents.push(head);
-    buildRelationship(child);
-  });
+const buildRelationship = (node: GraphNode) => {
+  const built = new Set<string>();
+  const traverseChildren = (node: GraphNode) => {
+    node.children.forEach((child) => {
+      child.parents.push(node);
+      if (built.has(child.id)) return;
+      built.add(child.id);
+      traverseChildren(child);
+    });
+  };
+  traverseChildren(node);
 };
 
 const drawNodes = (head: GraphNode): NodeRenderer[] => {
@@ -101,6 +95,7 @@ const drawNodes = (head: GraphNode): NodeRenderer[] => {
       y: currentNode.y,
       active: true,
       branches: currentNode.branches,
+      node: currentNode,
     });
     currentNode.children.forEach((child) => nodes.push(child));
   }
@@ -109,6 +104,7 @@ const drawNodes = (head: GraphNode): NodeRenderer[] => {
 
 const drawEdges = (head: GraphNode): EdgeRenderer[] => {
   const output: EdgeRenderer[] = [];
+  const drawn = new Set<string>([]);
   // get edges for children
   const traverseChildren = (node: GraphNode, depth: number) => {
     const { endPoints } = node;
@@ -117,23 +113,73 @@ const drawEdges = (head: GraphNode): EdgeRenderer[] => {
       const edgeStart = endPoints[Math.min(index, numEndPoints - 1)];
       const edgeEnd: Coord = { x: child.x, y: child.y };
       output.push({
-        id: `${node.id}-${child.id}`,
+        id: `child-${node.id}-${child.id}`,
         start: edgeStart,
         end: edgeEnd,
         depth,
       });
+      if (drawn.has(child.id)) return;
+      drawn.add(child.id);
       traverseChildren(child, depth + 1);
     });
   };
 
+  const traverseParents = (node: GraphNode, depth: number) => {
+    const { x: startX, y: startY, id } = node;
+    const edgeStart: Coord = { x: startX, y: startY };
+    node.parents.forEach((parent) => {
+      const nthChild = parent.children.findIndex((child) => child.id == id);
+      const coord = parent.endPoints[Math.min(nthChild, parent.endPoints.length - 1)];
+      const edgeEnd: Coord = { x: coord.x, y: coord.y };
+      output.push({
+        id: `parent-${id}-${parent.id}`,
+        start: edgeStart,
+        end: edgeEnd,
+        depth,
+      });
+      if (drawn.has(parent.id)) return;
+      drawn.add(parent.id);
+      traverseParents(parent, depth + 1);
+    });
+  };
+
   traverseChildren(head, 0);
+  // drawn.clear();
+  traverseParents(head, 0);
   return output;
 };
+
+// create nodes
+const head = new GraphNode("head", "io", 100, 300, []);
+const chat1 = new GraphNode("chat1", "chat", 440, 200, []);
+const branch1 = new GraphNode("branch1", "branch", 400, 450, [], 2);
+const state1 = new GraphNode("state1", "state", 560, 360, []);
+const json1 = new GraphNode("json1", "json", 720, 300, []);
+const lambda1 = new GraphNode("lambda1", "lambda", 1130, 280, []);
+const workflow1 = new GraphNode("workflow1", "workflow", 580, 520, []);
+const workflow2 = new GraphNode("workflow2", "workflow", 780, 100, []);
+const branch2 = new GraphNode("branch2", "branch", 820, 520, [], 3);
+const state2 = new GraphNode("state2", "state", 980, 420, []);
+const io2 = new GraphNode("io2", "io", 980, 720, []);
+const chat2 = new GraphNode("chat2", "chat", 1080, 560, []);
+
+//  create graph
+head.children = [chat1, branch1];
+branch1.children = [state1, workflow1];
+chat1.children = [json1, workflow2];
+state1.children = [json1];
+json1.children = [state2];
+workflow1.children = [branch2];
+branch2.children = [state2, chat2, io2];
+workflow2.children = [lambda1];
+state2.children = [lambda1];
 
 // build graph and draw nodes
 buildRelationship(head);
 const nodeRenderers = drawNodes(head);
 const nodeEdges = drawEdges(head);
 
-export { nodeRenderers, nodeEdges };
+const animatedNodeEdges = drawEdges(json1);
+
+export { nodeRenderers, nodeEdges, animatedNodeEdges, drawEdges };
 export type { NodeRenderer };
